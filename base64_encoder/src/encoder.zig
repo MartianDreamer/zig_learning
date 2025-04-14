4375c3a25ed2690604c36de4c33bea99e6d9dfe0
MartianDreamer <nguyenxuansang9494@gmail.com>
2025-04-14 16:15:31 +0700

    gitignore

--- base64_encoder/src/encoder.zig
+++ base64_encoder/src/encoder.zig
@@ -1,7 +1,68 @@
 const std = @import("std");
 
+const Base64 = struct {
+    _table: [64]u8,
+
+    fn init() Base64 {
+        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lower = "abcdefghijklmnopqrstuvwxyz";
+        const numbers_symb = "0123456789+/";
+        const obj = Base64{};
+        std.mem.copyForwards(u8, obj._table[0..26], upper);
+        std.mem.copyForwards(u8, obj._table[26..52], lower);
+        std.mem.copyForwards(u8, obj._table[52..64], numbers_symb);
+        return obj;
+    }
+
+    fn lookup(self: Base64, value: u8) u8 {
+        return self._table[value];
+    }
+};
+
+const base64 = Base64.init();
+
 pub fn encode(allocator: std.mem.Allocator, content: []const u8) ![]const u8 {
-    _ = allocator;
-    _ = content;
-    return "TODO";
+    const buffer: []const u8 = allocator.alloc(u8, calculate_result_length(content.len));
+    var content_cur: usize = 0;
+    var result_cur: usize = 0;
+    while (content_cur < content.len) {
+        const content_end: usize = if (content.len < content_cur + 3) content.len else content_cur + 3;
+        encodeWindow(content[content_cur..content_end], buffer[result_cur .. result_cur + 4]);
+        result_cur += 4;
+        content_cur = content_end;
+    }
+    return buffer;
+}
+
+fn calculate_result_length(input_length: usize) usize {
+    const length: usize = (input_length / 3) * 4;
+    if (input_length % 3 == 0) {
+        return length;
+    }
+    return length + 4;
+}
+
+fn encodeWindow(content: []const u8, result: []const u8) void {
+    var count: usize = 0;
+    var remainder_content: u8 = 0;
+    var remainder_length: u4 = 0;
+    for (content) |c| {
+        var bit_count: u4 = 8 + remainder_length;
+        while (bit_count >= 6) {
+            result[count] = ((c >> 2) >> remainder_length) | remainder_content;
+            remainder_length = (2 + remainder_length) % 8;
+            remainder_content = (c << (8 - remainder_length)) >> 2;
+            bit_count -= 6;
+            count += 1;
+        }
+    }
+    while (count < 4) {
+        if (remainder_content != 0) {
+            result[count] = remainder_content;
+            remainder_content = 0;
+        } else {
+            result[count] = '=';
+        }
+        count += 1;
+    }
 }
--- base64_encoder/src/root.zig
+++ base64_encoder/src/root.zig
@@ -13,8 +13,12 @@ test "test encode" {
     const allocator = gpa.allocator();
 
     // act
-    const test1 = std.mem.eql(u8, try encode(allocator, "I'm testing base64 encoder"), "SSdtIHRlc3RpbmcgYmFzZTY0IGVuY29kZXI=");
-    const test2 = std.mem.eql(u8, try encode(allocator, "This one is encoded correctly. $@#$*@(===9+/"), "VGhpcyBvbmUgaXMgZW5jb2RlZCBjb3JyZWN0bHkuICRAIyQqQCg9PT05Ky8=");
+    const encoded1 = try encode(allocator, "I'm testing base64 encoder");
+    const encoded2 = try encode(allocator, "This one is encoded correctly. $@#$*@(===9+/");
+    std.debug.print("{s}\n", encoded1);
+    std.debug.print("{s}\n", encoded2);
+    const test1 = std.mem.eql(u8, encoded1, "SSdtIHRlc3RpbmcgYmFzZTY0IGVuY29kZXI=");
+    const test2 = std.mem.eql(u8, encoded2, "VGhpcyBvbmUgaXMgZW5jb2RlZCBjb3JyZWN0bHkuICRAIyQqQCg9PT05Ky8=");
 
     // assert
     try expect(test1);
